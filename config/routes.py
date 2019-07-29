215e4b4d33bd73ccc1e33b0f393f77bba269a940
gaebar <gaetanobarreca@gmail.com>
2019-07-29 19:18:53 +0100

    Ready for deployment

--- config/routes.py
+++ config/routes.py
@@ -3,3 +3,14 @@ from controllers import stories, auth
 
 app.register_blueprint(stories.api, url_prefix='/api')
 app.register_blueprint(auth.api, url_prefix='/api')
+
+
+@app.route('/', defaults={'path': ''})  # homepage
+@app.route('/<path:path>')  # any other path
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path):  # if path is a file, send it back
+        return app.send_static_file(path)
+
+    # otherwise send back the index.html file
+    return app.send_static_file('index.html')
