786fdcd3068f487b933a9ed8a8b8dff9deb836e5
Gaetano Barreca <gaetanobarreca@gmail.com>
2019-09-16 18:09:33 +0100

    added new seed

--- seeds.py
+++ seeds.py
@@ -72,7 +72,11 @@ with app.app_context():
     if errors:
         raise Exception(errors)
 
-    second_story = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    second_story = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ' + \
+        'ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' + \
+        'ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum' + \
+        'dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia' + \
+        'deserunt mollit anim id est laborum.'
 
     lorem = Story(
         title='The standard Lorem Ipsum passage, used since the 1500s',
